7e984df1b1f47653d82e935e2d3e1d9d675c5154
Peter Kim <kroits09@gmail.com>
2018-06-15 16:23:22 +0900

    Add react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const routes = (
+  <BrowserRouter>
+
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
